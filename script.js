864c47977ff26ca48560ce1fa469edcb2b53d36e
Aarohi-006 <lala934@newschool.edu>
2026-04-07 20:47:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-async function getAdvice
-    const data = (await fetch{'	https://api.adviceslip.com/advice'}).json();
-}
+async function getAdvice() {
+    const data = await (await fetch('https://api.adviceslip.com/advice')).json();
 
 document.getElementById('text').innerText = data.slip.advice;
+} 
 
 getAdvice();
